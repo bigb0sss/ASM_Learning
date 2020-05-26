e64ef474d0ed946f11233ba8aceef834ce2a5195
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2020-05-25 22:25:59 -0400

    Update print_bigb0ss_3times.asm

--- 0x03_Loop/print_bigb0ss_3times.asm
+++ 0x03_Loop/print_bigb0ss_3times.asm
@@ -1,1 +1,39 @@
+; Executable name : print_bigb0ss_3times
+; Version         : 1.0
+; Created date    : 05/25/2020
+; Last update     : 05/25/2020
+; Author          : bigb0ss
+; Description.    : Print "bigb0ss" x3 times
+;
+; Purpose:
 
+global _start
+
+section .text
+_start:
+        jmp entry
+        
+entry:
+        mov eax, 0x3        ; Setting EAX to 0x3 for our loop purposes
+        
+print:
+        push eax            ; Storing the current state of EAX
+        
+        mov eax, 0x4        ; Calling write() syscall
+        mov ebx, 1
+        mov ecx, message
+        mov edx, mlen
+        int 0x80
+        
+        pop eax             ; Restoring the EAX state
+        dec eax             ; Decrementing by 1
+        jnz print           ; If the EAX is not equal to 0, then go back to print:
+        
+        mov eax, 0x1        ; Calling exit() syscall
+        mov ebx, 0xa
+        int 0x80
+
+section .data
+
+        message: db "bigb0ss "
+        mlen     equ $-message
